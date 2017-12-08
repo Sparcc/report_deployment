cc08eddc48262bc5bfff9755f929b84e0cf205ff
avih8r <sparco.209@gmail.com>
2017-12-08 16:09:03 +1100

    Replacement of hipchat login to message using hipchat api

--- report_deployment.py
+++ report_deployment.py
@@ -12,7 +12,27 @@ import configparser
 import re
 
 def str2bool(v):
-  return v.lower() in ("yes", "true", "t", "1")
+	return v.lower() in ("yes", "true", "t", "1")
+
+class HipchatApiInterface():
+	authToken = ''
+	def __init__(self):
+		config = configparser.ConfigParser()
+		config.read('config.ini')
+		self.authToken = config['HipchatAPI']['authToken']
+		
+	def sendMessage(self, message, roomId):
+		url = 'https://api.hipchat.com/v2/room/%d/notification' % roomId
+		headers = {
+			"content-type": "application/json",
+			"authorization": "Bearer %s" % self.token}
+		datastr = json.dumps({
+			'message': message,
+			'color': 'yellow',
+			'message_format': 'html',
+			'notify': True})
+		r = requests.post(url, headers=headers, data=datastr)
+		r.raise_for_status()
 
 class Environment:
 	driverPaths ={}
@@ -28,6 +48,7 @@ class Environment:
 	loginScriptLocation = ""
 	loginArgs = ""
 	passwd = ""
+	hipchatHandler = 0
 	
 	def __init__(self, passwd):
 		self.driverPaths = {'Chrome': 'C:\Selenium\chromedriver.exe',
@@ -35,10 +56,10 @@ class Environment:
 				'IE': 'C:\Selenium\IEDriverServer.exe'
 				};
 		
-		self.room = {'TEST-NOTIF': 'https://rxpservices.hipchat.com/chat/room/4292319',
-				'CCA': 'https://rxpservices.hipchat.com/chat/room/3554226',
-				'TEST': 'https://rxpservices.hipchat.com/chat/room/4216688',
-				'DEFAULT': 'https://rxpservices.hipchat.com/chat/room/4216688',
+		self.room = {'TEST-NOTIF': '4292319',
+				'CCA': '3554226',
+				'TEST': '4216688',
+				'DEFAULT': '4216688',
 				'NONE': ''
 				};
 		
@@ -83,48 +104,20 @@ def login (driver, usr, passwd):
 	xpath = '//*[@id="loginForm_save"]'
 	driver.find_element_by_xpath(xpath).click()
 
-def enterMessage(driver, xpath, message):
-	element = driver.find_element_by_xpath(xpath)
-	element.send_keys(message)
-	time.sleep(1)
-	element.send_keys(Keys.RETURN)
-	time.sleep(1)
+def enterMessage(driver, message):
+	hipHandler = HipchatApiInterface()
+	
+	hiphandler.sendMessage(message, roomId)
+	
 
-def reportToHipchat(driver, message, url, usr, passwd, designatedRooms, mainRoom, notificationOnlyMessage):
-	
-	driver.get(url)
-	
-	xpath = '//*[@id="email"]'
-	element = driver.find_element_by_xpath(xpath)
-	element.send_keys(usr)
-	xpath = '//*[@id="signin"]'
-	driver.find_element_by_xpath(xpath).click()
-	
-	xpath = '//*[@id="password"]'
-	element = driver.find_element_by_xpath(xpath)
-	element.send_keys(passwd)
-	xpath = '//*[@id="signin"]'
-	driver.find_element_by_xpath(xpath).click()
-	
-	#proceed to web app
-	xpath = '//*[@id="content"]/div/div/div/div[1]/div/a'
-	driver.find_element_by_xpath(xpath).click()
-	
-	#wait for web app to load
-	driver.implicitly_wait(10)
-	
-	#select room
-	for room in designatedRooms:
-		driver.get(room)
-		#wait for web app to load
-		time.sleep(5)
-		
-		#enter message
-		xpath = '//*[@id="hc-message-input"]'
-		if room == mainRoom:
-			enterMessage(driver, xpath, notificationOnlyMessage)
-		else:
-			enterMessage(driver, xpath, message)
+def reportToHipchat(message, designatedRooms, mainRoom, notificationOnlyMessage, roomId):
+	for room in designatedRooms: 
+		#wait for web app to load 
+		time.sleep(5) 
+		if room == mainRoom: 
+			enterMessage(driver, notificationOnlyMessage, roomId[room]) 
+		else: 
+			enterMessage(driver, message)
 		
 		
 def listJiraTickets (driver, url, specificBranch):
@@ -134,6 +127,7 @@ def listJiraTickets (driver, url, specificBranch):
 	ticketCount = 1
 	message = ""
 	ticketFound = False
+	message += '<ul style="list-style-type:disc">'
 	while ticketCount <= maxHtmlElements:
 		xpath = '//*[@id="fullChanges"]/div/ul/li['+str(ticketCount)+']/p/a'
 		xpath2 = '//*[@id="fullChanges"]/div/ul/li['+str(ticketCount)+']/p'
@@ -147,9 +141,15 @@ def listJiraTickets (driver, url, specificBranch):
 			descriptionMatch = re.search('\](.*)', descriptionElement.text)
 			description = descriptionMatch.group()
 			description = description[2:]
-			message += (ticket.get_attribute('data-issue-key') + '\n' +
-						' Link - ' + ticket.get_attribute('href') + '\n' +
-						'Description - ' + description + '\n' +
+			message += ('<ul style="list-style-type:disc">' +
+							'<li>' +
+							ticket.get_attribute('data-issue-key') +
+							'<ul style="list-style-type:disc">' +
+								'<li>Link - <a href="'+ ticket.get_attribute('href') + '">Click Here</a></li>' +
+								'<li>Description - ' + description + '</li>' +
+							'</ul>' +
+							'</li>' +
+						'</ul>' +
 						'-------------------------------------------------'
 						)
 		ticketCount += 1
@@ -209,12 +209,10 @@ def beginMonitoring (env):
 			driver.get(driver.current_url)
 		#after success found then a message is posted to hipchat6
 		specificBranch = env.lastBranchDeployed.lstrip('buildResult_')
-		notificationOnlyMessage = (env.message + ' ' + specificBranch + '\n' + 
-								'-------------------------------------------------\n')
-		env.message += (' ' + specificBranch + '\n' + 
-								'-------------------------------------------------\n')
+		notificationOnlyMessage = (env.message + '<strong>' + specificBranch + '</strong><br>' + '<hr>')
+		env.message += (' <strong>' + specificBranch + '</strong><br>' + '<hr>')
 		env.message += listJiraTickets(driver, env.url, specificBranch)
-		reportToHipchat(driver, env.message, env.url['hipchat'], env.usr['hipchat'], env.passwd, env.designatedRooms, env.mainRoom, notificationOnlyMessage)
+		reportToHipchat(env.message, env.designatedRooms, env.mainRoom, notificationOnlyMessage, env.room)
 		if env.login:
 			os.chdir(env.loginScriptLocation)
 			os.system('python CCA_autologin.py ' + env.loginArgs)
